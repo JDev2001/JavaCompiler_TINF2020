5c9cff8ffc691ba388d80753032984e2669a39fd
JDev2001 <32664292+JDev2001@users.noreply.github.com>
2022-05-09 15:49:29 +0200

    .

--- Project/DataClasses/src/Statements/MethodCallStatement.java
+++ Project/DataClasses/src/Statements/MethodCallStatement.java
@@ -2,6 +2,8 @@ package Statements;
 
 import Expressions.IExpression;
 
+import java.util.List;
+
 /**
  * target.name();
  */
@@ -9,11 +11,18 @@ public class MethodCallStatement implements IStatement
 {
     private String name;
     private IExpression target;
+    private List<IExpression> parameters;
 
-    public MethodCallStatement(String name, IExpression target)
+    public MethodCallStatement(String name, IExpression target, List<IExpression> parameters)
     {
         this.name = name;
         this.target = target;
+        this.parameters = parameters;
+    }
+
+    public List<IExpression> getParameters()
+    {
+        return parameters;
     }
 
     public String getName()
