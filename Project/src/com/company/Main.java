d5004881032bceefd4f626255138a0dc086062d1
SuuyaaYeet <suuyaa@protonmail.com>
2022-05-09 12:12:40 +0200

    Update UML Test Main

--- Project/src/com/company/Main.java
+++ Project/src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
-    }
+        Object i  = new A();
+            switch (i)
+            {
+                case A a  -> System.out.println("A");
+                case B a  -> System.out.println("B");
+                case Object o -> System.out.println("default");
+            }
+        }
+ }
+
+class A
+{
+
+}
+
+class B
+{
+
 }
