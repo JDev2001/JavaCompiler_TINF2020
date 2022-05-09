87cf56867c74adb0021a812ee87c0ddaf8f4ac61
JDev2001 <32664292+JDev2001@users.noreply.github.com>
2022-05-09 15:57:22 +0200

    .

--- Project/DataClasses/src/Expressions/BinaryExpression.java
+++ Project/DataClasses/src/Expressions/BinaryExpression.java
@@ -7,11 +7,18 @@ public class BinaryExpression implements IExpression
 {
     private IExpression a;
     private IExpression b;
+    private String operator;
 
-    public BinaryExpression(IExpression a, IExpression b)
+    public BinaryExpression(IExpression a, IExpression b, String operator)
     {
         this.a = a;
         this.b = b;
+        this.operator = operator;
+    }
+
+    public String getOperator()
+    {
+        return operator;
     }
 
     public IExpression getA()
