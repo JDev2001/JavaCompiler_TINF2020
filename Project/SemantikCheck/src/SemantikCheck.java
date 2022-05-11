f6b247017c55ce3b3fcd38aefcfc5f6d8d9f86f2
jan510 <i20027@hb.dhbw-stuttgart.de>
2022-05-11 11:51:34 +0200

    Update SemantikCheck.java

--- Project/SemantikCheck/src/SemantikCheck.java
+++ Project/SemantikCheck/src/SemantikCheck.java
@@ -4,8 +4,7 @@ import Statements.*;
 import typedCommon.*;
 
 public interface SemantikCheck {
-
-    TypedAccessModfiers
+    
     TypedClass semantikCheck(Class untyped);
 
     TypedIfElseStatement semantikCheck(IfElseStatement untyped);
