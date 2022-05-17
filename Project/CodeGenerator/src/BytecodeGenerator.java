ac206bf78af585b8af163e51b7d3d52ccd6b8c09
Suuyaa <suuyaa@protonmail.com>
2022-05-17 14:36:15 +0200

    added fields

--- Project/CodeGenerator/src/BytecodeGenerator.java
+++ Project/CodeGenerator/src/BytecodeGenerator.java
@@ -1,8 +1,11 @@
+import Common.AccessModifiers;
 import Common.Class;
 import Common.Program;
 import Field.Field;
 import Method.Method;
+import Types.*;
 import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.FieldVisitor;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import typedExpressions.*;
@@ -11,8 +14,13 @@ import typedStatementExpression.*;
 
 import java.util.List;
 
+import static Common.AccessModifiers.*;
+
 public class BytecodeGenerator {
     private static Program aProgram;
+    private ClassWriter cw;
+    private MethodVisitor methodVisitor;
+    private FieldVisitor fieldVisitor;
 
     public BytecodeGenerator(Program pProgram) {
         aProgram = pProgram;
@@ -28,10 +36,13 @@ public class BytecodeGenerator {
 
     private byte[] generateClassCode(Class pClass) {
         //Initiate class
-        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
+        cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
+        MethodVisitor methodVisitor;
+        FieldVisitor fieldVisitor;
         cw.visit(Opcodes.V18, Opcodes.ACC_SUPER, pClass.identifier(), null, "java/lang/Object", null);
 
         //visit Fields first
+        generateFieldCode(pClass.fields());
 
         //visit Constructors
         generateConstructors(cw, pClass.constructors());
@@ -44,29 +55,63 @@ public class BytecodeGenerator {
         return classBytecode;
     }
 
+    private void generateFieldCode(List<Field> pField) {
+        for (Field field : pField) {
+            int accessmod = -1;
+            String descriptor;
+            switch (field.accessModifiers()) {
+                case Public:
+                    accessmod = Opcodes.ACC_PUBLIC;
+                    break;
+                case Private:
+                    accessmod = Opcodes.ACC_PRIVATE;
+                    break;
+                case Protected:
+                    accessmod = Opcodes.ACC_PROTECTED;
+                    break;
+                default: throw new IllegalStateException("Unexpected value: " + field.type());
+            }
+            switch (field.type()) {
+                case BoolType type -> {
+                    descriptor = "B";
+                }
+                case IntType type -> {
+                    descriptor = "I";
+                }
+                case CharType type -> {
+                    descriptor = "C";
+                }
+                case CustomType type -> {
+                    descriptor = "L" + type.getName();
+                }
+                default -> throw new IllegalStateException("Unexpected value: " + field.type());
+            }
+
+            fieldVisitor = cw.visitField(accessmod, field.name(), descriptor, null, null);
+            fieldVisitor.visitEnd();
+        }
+    }
+
     private void generateConstructors(ClassWriter cw, List<Method> pConstructors) {
-        if(pConstructors.isEmpty()) {
+        if (pConstructors.isEmpty()) {
             //No constructors means standard constructor
-            MethodVisitor methodVisitor;
             methodVisitor = cw.visitMethod(Opcodes.ACC_PUBLIC, "<init>", "()V", null, null);
             methodVisitor.visitCode();
             methodVisitor.visitVarInsn(Opcodes.ALOAD, 0);
             methodVisitor.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
             methodVisitor.visitInsn(Opcodes.RETURN);
-            methodVisitor.visitMaxs(1, 1);
+            methodVisitor.visitMaxs(0, 0);
             methodVisitor.visitEnd();
         } else {
-            for(Method pMethod : pConstructors) {
-
+            //TODO
+            for (Method pMethod : pConstructors) {
+//                methodVisitor = cw.visitMethod();
+//                methodVisitor.visitMaxs(0,0);
             }
         }
     }
 
     private void generateMethodCode(Method pMethod, ClassWriter cw) {
-
-    }
-
-    private void generateFieldCode(Field pField) {
 
     }
 
@@ -82,23 +127,6 @@ public class BytecodeGenerator {
                 System.out.println(statement);
             }
             case TypedWhileStatement statement -> {
-                System.out.println(statement);
-            }
-            default -> {
-
-            }
-        }
-    }
-
-    private void generateStatementExpressions(ITypedStatementExpression pStatementExpression) {
-        switch (pStatementExpression) {
-            case TypedAssignStatementExpression statement -> {
-                System.out.println(statement);
-            }
-            case TypedMethodCallStatementExpression statement -> {
-                System.out.println(statement);
-            }
-            case TypedNewStatementExpression statement -> {
                 System.out.println(statement);
             }
             default -> {
@@ -135,6 +163,23 @@ public class BytecodeGenerator {
             }
             case TypedUnaryExpression expression -> {
                 System.out.println(expression);
+            }
+            default -> {
+
+            }
+        }
+    }
+
+    private void generateStatementExpressions(ITypedStatementExpression pStatementExpression) {
+        switch (pStatementExpression) {
+            case TypedAssignStatementExpression statement -> {
+                System.out.println(statement);
+            }
+            case TypedMethodCallStatementExpression statement -> {
+                System.out.println(statement);
+            }
+            case TypedNewStatementExpression statement -> {
+                System.out.println(statement);
             }
             default -> {
 
--- Project/CodeGenerator/src/Main.java
+++ Project/CodeGenerator/src/Main.java
@@ -1,9 +1,3 @@
-import Common.Class;
-import Field.Field;
-
-import java.util.List;
-
 public class Main {
-    public static void main(String[] args) {
-    }
+
 }
