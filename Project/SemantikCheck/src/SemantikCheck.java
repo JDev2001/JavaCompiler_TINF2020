9ac3ff3b11afd2f66148ccddb770b8bbef1630be
jan510 <i20027@hb.dhbw-stuttgart.de>
2022-05-18 14:41:59 +0200

    Added semantikCheck method template

--- Project/SemantikCheck/src/SemantikCheck.java
+++ Project/SemantikCheck/src/SemantikCheck.java
@@ -14,25 +14,25 @@ public interface SemantikCheck {
     ITypedStatement checkStatement(IStatement statement) throws Exception;
     ITypedStatementExpression checkStatementExpression(IStatementExpression statementExpression) throws Exception;
 
-    TypedBlock semantikCheck(Block untyped);
-    TypedClass semantikCheck(Class untyped);
+    TypedBlock semantikCheck(Block untyped) throws Exception;
+    TypedClass semantikCheck(Class untyped) throws Exception;
 
-    TypedBinaryExpression semantikCheck(BinaryExpression untyped);
-    TypedConstExpression semantikCheck(ConstExpression untyped);
-    TypedJNullExpression semantikCheck(JNullExpression untyped);
-    TypedSuperExpression semantikCheck(SuperExpression untyped);
-    TypedThisExpression semantikCheck(ThisExpression untyped);
-    TypedTypeExpression semantikCheck(TypeExpression untyped);
-    TypedUnaryExpression semantikCheck(UnaryExpression untyped);
+    TypedBinaryExpression semantikCheck(BinaryExpression untyped) throws Exception;
+    TypedConstExpression semantikCheck(ConstExpression untyped) throws Exception;
+    TypedJNullExpression semantikCheck(JNullExpression untyped) throws Exception;
+    TypedSuperExpression semantikCheck(SuperExpression untyped) throws Exception;
+    TypedThisExpression semantikCheck(ThisExpression untyped) throws Exception;
+    TypedTypeExpression semantikCheck(TypeExpression untyped) throws Exception;
+    TypedUnaryExpression semantikCheck(UnaryExpression untyped) throws Exception;
 
-    TypedAssignStatementExpression semantikCheck(AssignStatementExpression untyped);
-    TypedInstVarStatementExpression semantikCheck(InstVarStatementExpression untyped);
-    TypedMethodCallStatementExpression semantikCheck(MethodCallStatementExpression untyped);
-    TypedNewStatementExpression semantikCheck(NewStatementExpression untyped);
+    TypedAssignStatementExpression semantikCheck(AssignStatementExpression untyped) throws Exception;
+    TypedInstVarStatementExpression semantikCheck(InstVarStatementExpression untyped) throws Exception;
+    TypedMethodCallStatementExpression semantikCheck(MethodCallStatementExpression untyped) throws Exception;
+    TypedNewStatementExpression semantikCheck(NewStatementExpression untyped) throws Exception;
 
     TypedIfElseStatement semantikCheck(IfElseStatement untyped) throws Exception;
-    TypedReturnStatement semantikCheck(ReturnStatement untyped);
-    TypedVarDeclarationStatement semantikCheck(VarDeclarationStatement untyped);
+    TypedReturnStatement semantikCheck(ReturnStatement untyped) throws Exception;
+    TypedVarDeclarationStatement semantikCheck(VarDeclarationStatement untyped) throws Exception;
     TypedWhileStatement semantikCheck(WhileStatement untyped) throws Exception;
 }
 
