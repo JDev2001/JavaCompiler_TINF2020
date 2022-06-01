02ac54a8ec2a66121a34851f8117e09586ce2a9e
JDev2001 <32664292+JDev2001@users.noreply.github.com>
2022-06-01 21:40:59 +0200

    Added FunctionTest

--- Project/Tests/FunctionalTest/MainProg.java
+++ Project/Tests/FunctionalTest/MainProg.java
@@ -3,6 +3,7 @@ public class MainProg
     public static void main(String[] args)
     {
         MyClass myClass = new MyClass();
+        System.out.println(myClass.GetData(3,true));
         System.out.println(myClass.GetData(1,false));
     }
 }
