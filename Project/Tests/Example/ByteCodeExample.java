970edc8839916558d74a3be3a6b967c94f77b66f
Suuyaa <suuyaa@protonmail.com>
2022-06-01 20:39:33 +0200

    Example updated

---
+++ Project/Tests/Example/ByteCodeExample.java
@@ -0,0 +1,9 @@
+public class ByteCodeExample
+{
+    public static void main(String[] args)
+    {
+        ByteCodeExampleClass test = new ByteCodeExampleClass();
+        System.out.println("Tests j: "+test.whatsMyj());
+        System.out.println("Tests h + 1: "+test.moreThanh());
+    }
+}
